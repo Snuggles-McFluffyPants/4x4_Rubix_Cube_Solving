30431390d3ba381b3e084effe1350f9ee9b519c2
Snuggles-McFluffyPants <68079565+Snuggles-McFluffyPants@users.noreply.github.com>
2021-08-09 13:24:08 -0400

    Description of program added to main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+"""
+The following program is something to help solve the final bit of
+a 4x4 rubix cube where all but 2 cubes are in the right place
+
+This program takes an original list with 8 items in it to represent
+blocks that may need to be moved around. The final result of this program
+should be the original list with all but 2 list items back in their
+original place along with the solution with the least number of steps
+to get there
+"""
+
 original_list = [1,2,3,4,5,6,7,8]
 
 
